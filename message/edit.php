940fc665ca9228830bc1dd888ff4b2c9ffe64457
codebuilder2019 <gjaime@unicauca.edu.co>
2023-04-15 16:51:27 -0500

    The edit form saves records in the DB

--- message/edit.php
+++ message/edit.php
@@ -8,11 +8,26 @@ $PAGE->set_url(new moodle_url('/local/message/edit.php'));
 $PAGE->set_context(\context_system::instance()); // The global context will be used
 $PAGE->set_title('Edit Page');
 
-// We want to display our form.
 $mform = new edit();
 
+if ($mform->is_cancelled()) {
+    // Go back to manage.php page
+    redirect($CFG->wwwroot . '/local/message/manage.php', 'The message creation was cancelled');
+
+} else if ($fromform = $mform->get_data()) {
+
+    // Insert the data in the DB
+    $recordToInsert = new stdClass();
+    $recordToInsert->messagetext = $fromform->messagetext;
+    $recordToInsert->messagetype = $fromform->messagetype;
+
+    // Parameters: Table name, new record;
+    $DB->insert_record('local_message', $recordToInsert);
+
+    // Go back to manage.php page
+    redirect($CFG->wwwroot . '/local/message/manage.php', 'The message '. $fromform->messagetext. ' was created');
+}
+
 echo $OUTPUT->header();
-
 $mform->display();
-
 echo $OUTPUT->footer();
